896c114a32306af576061a54b3518915a54751de
Youri Schuurmans <yourish@live.nl>
2017-07-13 15:17:36 +0200

    fancyfied the code a bit

--- Scripts/HallyGally.lua
+++ Scripts/HallyGally.lua
@@ -1,3 +1,5 @@
+color = ''
+
 function onload()
   local button = {}
   button.width = 1500
@@ -10,21 +12,16 @@ function onload()
   self.createButton(button)
 end
 
-color = ''
-
 function buttonClicked(clickedObject, player_color)
-  -- printToAll('('..player_color..')' .. Player[player_color].steam_name .. ' clicked the button',stringColorToRGB(player_color))
   if(Timer.destroy('turnTimer') == false )
   then
     color = player_color
   end
-
   newTimer()
 end
 
 function endTurn()
-  --printToAll('-- End of turn -- ',{1,1,1})
-  printToAll(Player[color].steam_name .. ' clicked the button first!',stringColorToRGB(color))
+  printToAll(Player[color].steam_name .. ' clicked the button first!', stringColorToRGB(color))
 end
 
 function newTimer()
